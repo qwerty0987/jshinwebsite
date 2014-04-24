e397c16a13733d2a97abaec2ef0ad008aa077a92
Estelle Shin <estelleshin@hotmail.com>
2014-04-23 20:31:57 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jshinwebsite::Application.config.secret_key_base = '7bd95c368182005e368d4a7c3e1ea4fa4ca17c359859e47af70f3a8ac17944ca33aa0e953928c14b31fa8d3dbc9e3d465c73d2fcac451565deb8455d9c8fb2f6'
